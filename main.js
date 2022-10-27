9fc2d737aa68bfaced6d750ca11757f08e227b6e
Bianca <bianc@LAPTOP-4GPQTJ00>
2022-10-27 15:59:41 -0300

    exercicio-html-js

--- main.js
+++ main.js
@@ -1,62 +1,48 @@
 const form = document.getElementById('form-enviar');
-const nomeCompleto = document.getElementById('nome-interessado');
+
 let formEValido = false;
 
-function validaNome(nomeCompleto) {
-    const nomeComoArray = nomeCompleto.split(' ');
-    return nomeComoArray.length >= 2;
+function validaCampoA(campoA) {
+    const campoAComoArray = campoA.split(' ');
+    return campoAComoArray.length >campoA ;
 }
 
-function validaSenha(Senha) {
-    const senhaComoArray = senha.split(' ');
-    return senhaComoArray.length >= 1;
-}
-
-function validaidade(idade) {
-    const idadeComoArray = idade.split(' ');
-    return idadeComoArray.length >= 1;
+function validaCampoB(campoB) {
+    const campoBComoArray = campoB.split(' ');
+    return campoBComoArray.length  <campoB ;
 }
 
 form.addEventListener('submit', function(e) {
      e.preventDefault();
-
-    const senha = document.getElementById('numero-senha');
-    const idade = document.getElementById('numero-idade');
-    const mensagemSucesso = `Numero da idade: <b>${idade.value}</b> enviado por: <b>${nomeCompleto.value}</b> - Senha: <b>${senha.value}</b>`;
+     const CampoA = document.getElementById('campo-A');
+     const CampoB = document.getElementById('campo-B');
+     const mensagemSucesso = `Campo B: <b>${CampoB.value}</b> é maior que o campo A: <b>${CampoA.value}</b>`;
     
-    formEValido = validaNome(nomeCompleto.value)
+    formEValido = validaCampoA (CampoA.value)
     if (formEValido) {
         const containerMensagemSucesso = document.querySelector('.success-message');
         containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display = 'block';
+
  
-        nomeCompleto.value = '';
-        senha.value = '';
-        idade.value = '';
+        CampoA.value = '';
+        CampoB.value = '';
 
     } else {
-        nomeCompleto.style.border = '1px solid red';
+        CampoA.style.border = '1px solid red';
         document.querySelector('.error-message').style.display = 'block';
     }
 })
 
-nomeCompleto.addEventListener('keyup', function(e){
+    campoA.addEventListener('keyup', function(e){
     console.log(e.target.value);
-    formEValido = validaNome(e.target.value);
+    formEValido = validaCampoA(e.target.value);
 
     if (!formEValido) {
-        nomeCompleto.classList.add('error');
+        campoA.classList.add('error');
         document.querySelector('.error-message').style.display = 'block';
     } else {
-        nomeCompleto.classList.remove('error');
+        campoA.classList.remove('error');
         document.querySelector('.error-message').style.display = 'none';
-    }
-
-    if(campoidade > camposenha){
-        console.log("idade é maior que senha");
-     }else {
-         console.log("senha é maior que idade");
-         console.log("os dois são iguais");
-     }
+    };
 });
-
