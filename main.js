41850a20357ae773adae8559c78c6daaacff0955
Bianca <bianc@LAPTOP-4GPQTJ00>
2022-10-30 20:06:07 -0300

    exercicio

--- main.js
+++ main.js
@@ -2,36 +2,31 @@ const form = document.getElementById('form-enviar');
 
 let formEValido = false;
 
-function validaCampoA(campoA) {
-    const campoAComoArray = campoA.split(' ');
-    return campoAComoArray.length >campoA ;
-}
-
-function validaCampoB(campoB) {
-    const campoBComoArray = campoB.split(' ');
-    return campoBComoArray.length  <campoB ;
-}
-
+function validaCampoA(campoA,campoB) {
+  
+    if (campoB > campoA) {return campoA}
+    else {return campoB}
+    
+  }
+  
 form.addEventListener('submit', function(e) {
      e.preventDefault();
      const CampoA = document.getElementById('campo-A');
      const CampoB = document.getElementById('campo-B');
      const mensagemSucesso = `Campo B: <b>${CampoB.value}</b> é maior que o campo A: <b>${CampoA.value}</b>`;
     
-    formEValido = validaCampoA (CampoA.value)
-    if (formEValido) {
+     formEValido = validaCampoA (CampoA.value)
+
+    if (CampoA.value < CampoB.value) {
         const containerMensagemSucesso = document.querySelector('.success-message');
         containerMensagemSucesso.innerHTML = mensagemSucesso;
         containerMensagemSucesso.style.display = 'block';
-
- 
-        CampoA.value = '';
-        CampoB.value = '';
 
     } else {
         CampoA.style.border = '1px solid red';
         document.querySelector('.error-message').style.display = 'block';
     }
+
 })
 
     campoA.addEventListener('keyup', function(e){
